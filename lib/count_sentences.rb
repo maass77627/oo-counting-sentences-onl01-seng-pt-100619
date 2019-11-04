e2a229b2a921cbe51d2d6bd8576e458dcc11a52a
kelly r maass <kmaass77627@gmail.com>
2019-11-04 04:35:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    self.end_with?(".")
+   end
 
   def question?
-
-  end
+    self.end_with?("?")
+ end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\![!]*/).count 
   end
+
+
 end
